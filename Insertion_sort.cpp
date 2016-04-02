8b57891a4fbabf30f270b891e42d58b4f2549731
Nimish Joshi <jnimish77@users.noreply.github.com>
2016-04-01 20:13:06 -0500

    Create Insertion_sort.cpp

---
+++ Insertion_sort.cpp
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <iostream>
+ 
+using namespace std;
+ 
+//member function
+void insertion_sort(int arr[], int length);
+void print_array(int array[],int size);
+ 
+int main() {
+ 
+int array[20]= {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+insertion_sort(array,20);
+ 
+ return 0;
+
+}
+
+//end of main
+ 
+void insertion_sort(int arr[], int length) {
+ int i, j ,tmp;
+ for (i = 1; i < length; i++) {
+ j = i;
+ while (j > 0 && arr[j - 1] > arr[j]) {
+ tmp = arr[j];
+ arr[j] = arr[j - 1];
+ arr[j - 1] = tmp;
+ j--;
+ }//end of while loop
+ print_array(arr,20);
+ }//end of for loop
+ }//end of insertion_sort.
+ 
+void print_array(int array[], int size){
+ 
+ cout<< "sorting: ";
+ int j;
+ for (j=0; j<size;j++)
+ for (j=0; j<size;j++)
+ cout <<" "<< array[j];
+ cout << endl;
+ }
+//end of print_array
