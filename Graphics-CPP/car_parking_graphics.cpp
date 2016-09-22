93bb590f213b8b16501aa23aefceaa35ec65effc
Nimish Joshi <jnimish77@users.noreply.github.com>
2016-09-22 11:37:08 -0500

    Update car_parking_graphics.cpp

--- Graphics-CPP/car_parking_graphics.cpp
+++ Graphics-CPP/car_parking_graphics.cpp
@@ -115,18 +115,9 @@ float i, j=0;
        
        
        cleardevice();
-      
-      
     
-	
-	
-
-	
-	
 }
    
-    
-    
    for (i=0;i<=900;i=i+10, j++)
    {
    
@@ -154,32 +145,26 @@ float i, j=0;
    settextstyle(2,HORIZ_DIR,6); //here 2 means font_style, 6 means size of font
    outtextxy(450,140,"4");
    
-   
-   
+  
    moveto(500,150);
    lineto(500,30);
    settextstyle(2,HORIZ_DIR,6); //here 2 means font_style, 6 means size of font
    outtextxy(550,140,"5");
-   
    
    moveto(600,150);
    lineto(600,30);
    settextstyle(2,HORIZ_DIR,6); //here 2 means font_style, 6 means size of font
    outtextxy(650,140,"6");
    
-
-
    moveto(700,150);
    lineto(700,30);
    settextstyle(2,HORIZ_DIR,6); //here 2 means font_style, 6 means size of font
    outtextxy(750,140,"7");
-   
-   
+  
    moveto(800,150);
    lineto(800,30);
    settextstyle(2,HORIZ_DIR,6); //here 2 means font_style, 6 means size of font
    outtextxy(850,140,"8");
-   
    
    moveto(900,150);
    lineto(900,30);
@@ -187,18 +172,14 @@ float i, j=0;
    outtextxy(950,140,"9");
    
    
-   
    moveto(1000,150);
    lineto(1000,30);
    settextstyle(2,HORIZ_DIR,6); //here 2 means font_style, 6 means size of font
    outtextxy(1050,140,"10");
    
-   
     moveto(1100,150);
    lineto(1100,30);
-   
-   
-
+  
 	 
 		line(930,350-i,980,350-i);
 		line(980,350-i,980,250-i);
@@ -209,30 +190,15 @@ float i, j=0;
 	 if (i==210)
 	  break;
 	 
-	
-
-       
       // if(i==750)
       // rectangle(750,350-i,850-i,400);
        //rectangle(50,350-i,150,400-i);
        //break;
        
-       
        cleardevice();
-      
-      
-    
-	
-	
-
-	
-	
+     
 }
 	
-   
-   
-   
-   
    while(!kbhit());     
    closegraph(); 
    return 0; 
